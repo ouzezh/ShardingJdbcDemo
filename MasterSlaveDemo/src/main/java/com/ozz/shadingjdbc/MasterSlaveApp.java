4d14be96b032f2aa3c51b7b77dace49b53d48a20
ouzezh <04194f394c7bce93d4e654787a95b6c85222c62e>
2020-09-19 21:46:45 +0800

    init

---
+++ MasterSlaveDemo/src/main/java/com/ozz/shadingjdbc/MasterSlaveApp.java
@@ -0,0 +1,13 @@
+package com.ozz.shadingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSlaveApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MasterSlaveApp.class, args);
+  }
+
+}
---
+++ MasterSlaveDemo/src/test/java/com/ozz/shadingjdbc/MasterSlaveAppTests.java
@@ -0,0 +1,13 @@
+package com.ozz.shadingjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSlaveAppTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
