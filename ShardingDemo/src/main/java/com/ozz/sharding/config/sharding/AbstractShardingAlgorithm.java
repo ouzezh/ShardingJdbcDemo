5d46a42c6f58a8f20fe2196c02f88f2c5d8a9651
ouzezh <ouzezh@163.com>
2022-04-15 00:22:54 +0800

    u

--- ShardingDemo/src/main/java/com/ozz/sharding/config/sharding/AbstractShardingAlgorithm.java
+++ ShardingDemo/src/main/java/com/ozz/sharding/config/sharding/AbstractShardingAlgorithm.java
@@ -1,13 +1,13 @@
 package com.ozz.sharding.config.sharding;
 
-import java.util.Collection;
-import lombok.extern.slf4j.Slf4j;
+import cn.hutool.log.StaticLog;
 import org.apache.shardingsphere.api.sharding.ShardingValue;
 
-@Slf4j
+import java.util.Collection;
+
 public abstract class AbstractShardingAlgorithm {
   protected void printShadingInfo(Collection<String> availableTargetNames, ShardingValue shardingValue) {
-    log.info(String.format("\n[ShardingInfo] %s: availableTargetNames=%s, shardingValue=%s\n", getClass().getSimpleName(), availableTargetNames, shardingValue));
+    StaticLog.info(String.format("\n[ShardingInfo] %s: availableTargetNames=%s, shardingValue=%s\n", getClass().getSimpleName(), availableTargetNames, shardingValue));
   }
 
   protected String checkTargetName(Collection<String> availableTargetNames, String targetName) {
--- ShardingDemo/src/test/java/com/ozz/sharding/ShardingAppTests.java
+++ ShardingDemo/src/test/java/com/ozz/sharding/ShardingAppTests.java
@@ -1,8 +1,8 @@
 package com.ozz.sharding;
 
+import cn.hutool.log.StaticLog;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
-import com.ozz.sharding.model.TOrder;
 import com.ozz.sharding.model.TOrderItem;
 import com.ozz.sharding.service.MyService;
 import java.util.List;
@@ -31,24 +31,24 @@ class ShardingAppTests {
   private void testSelect() {
     // 广播表随机读一个库
     List<String> list = myService.selectSql("select name from t_dict");
-    System.out.println(list);
+    StaticLog.info(JSON.toJSONString(list));
     Assert.isTrue(list.size()==1, "select broadcast table error");
 
     // 未配置分片表读默认库
     list = myService.selectSql("select code from t_master_slave");
-    System.out.println(list);
+    StaticLog.info(JSON.toJSONString(list));
     Assert.isTrue(list.size()==1 && "mc".equals(list.get(0)), "select not match table error");
 
     // 配置分片未配置路由规则，读全部分片
     list = myService.selectSql("select 1 from t_hint_db_table");
-    System.out.println(list);
+    StaticLog.info(JSON.toJSONString(list));
     Assert.isTrue(list.size()==4, "select split and no route rule table error");
 
     // binding-tables
     // (1)如配置绑定表，执行2次查询，分别为： ds1-order0-item0, ds1-order1-item1
     // (2)如未配置绑定表，将执行2*2共4次查询，返回5条结果
     list = myService.selectSql("select i.order_item_id from t_order o join t_order_item i on o.user_id=i.user_id and o.order_id=i.order_id where o.user_id=?", 0L);
-    System.out.println(list);
+    StaticLog.info(JSON.toJSONString(list));
     Assert.isTrue(list.size()==2, "select binding-tables error");
   }
 
@@ -63,31 +63,31 @@ class ShardingAppTests {
 
   private void testShardingStandard() {
     List<String> list = myService.selectSql("select order_id from t_order where user_id=? and order_id=?", 1L, 3L);
-    System.out.println(JSON.toJSONString(list));
+    StaticLog.info(JSON.toJSONString(list));
     Assert.isTrue(list.size()==1 && "3".equals(list.get(0)), "check status fail");
   }
 
   private void testShardingHint() {
     List<String> list = myService.selectSql("select order_id from t_hint_db_table");
-    System.out.println(list);
+    StaticLog.info(JSON.toJSONString(list));
     Assert.isTrue(list.size()==4, "check status fail");
     try (HintManager hm = HintManager.getInstance()) {
       hm.setDatabaseShardingValue("ds1");// 仅匹配数据库 ( 清空 database & table 配置 )
       list = myService.selectSql("select order_id from t_hint_db_table");
-      System.out.println(list);
+      StaticLog.info(JSON.toJSONString(list));
       Assert.isTrue(list.size()==2, "check status fail");
     } finally {
       HintManager.clear();
     }
 
     list = myService.selectSql("select order_id from t_hint_db_table");
-    System.out.println(list);
+    StaticLog.info(JSON.toJSONString(list));
     Assert.isTrue(list.size()==4, "check status fail");
     try (HintManager hm = HintManager.getInstance()) {
       hm.addDatabaseShardingValue("t_hint_db_table", "ds1");
       hm.addTableShardingValue("t_hint_db_table", "t_order_1");
       list = myService.selectSql("select order_id from t_hint_db_table");
-      System.out.println(list);
+      StaticLog.info(JSON.toJSONString(list));
       Assert.isTrue(list.size()==1&&"3".equals(list.get(0)), "check status fail");
     } finally {
       HintManager.clear();
@@ -136,7 +136,7 @@ class ShardingAppTests {
     }
   }
   private void checkDynamicDataSource(List<String> res) {
-    System.out.println(res);
+    StaticLog.info(JSON.toJSONString(res));
     Assert.isTrue(res.size()==1 && "dc".equalsIgnoreCase(res.get(0)), "check status fail");
   }
 }
